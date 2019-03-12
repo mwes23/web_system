ae501dc95c8e3cd452a6e997d3c4b4af3cb351bb
kyeza <kyezaarnold63@gmail.com>
2019-03-12 11:17:49 +0300

    - Created two django apps payroll and staff - Setup MYSQL database called "hr_system_db"

--- hr_system/hr_system/settings.py
+++ hr_system/hr_system/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'payroll.apps.PayrollConfig',
+    'staff.apps.StaffConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'hr_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'read_default_file': 'C:\\Bitnami\\wampstack-7.1.27-0\\mysql\\data\\hr_db.cnf'
+        },
     }
 }
 
